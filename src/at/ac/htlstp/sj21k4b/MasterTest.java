d66b7c8f73389c945db9bafa844b0cad980dd598
Werner <werner.damboeck@letto.at>
2022-02-22 08:30:10 +0100

    Master Test

--- src/at/ac/htlstp/sj21k4b/MasterTest.java
+++ src/at/ac/htlstp/sj21k4b/MasterTest.java
@@ -1,4 +1,33 @@
 package at.ac.htlstp.sj21k4b;
 
+import java.io.File;
+import java.io.IOException;
+
 public class MasterTest {
+
+    public static void main(String[] args) {
+        File file = new File("data/test.txt");
+        if (file.exists()) {
+            System.out.println("File exists!");
+            file.delete();
+        } else {
+            try {
+                file.createNewFile();
+                System.out.println("File created!");
+            } catch (IOException e) {
+                File directory = new File("data");
+                if (directory.exists()) {
+                    System.out.println("File cannot be created!");
+                } else {
+                    directory.mkdirs();
+                    try {
+                        file.createNewFile();
+                    } catch (IOException ex) {
+                        System.out.println("File cannot be created!");
+                    }
+                }
+            }
+        }
+    }
+
 }
