e1bdfa6cb8c8146b7c9e8faa87d48794ca6ee43e
Werner <werner.damboeck@letto.at>
2022-03-08 08:57:12 +0100

    Files

--- src/at/ac/htlstp/sj21k4b/CSVsumme.java
+++ src/at/ac/htlstp/sj21k4b/CSVsumme.java
@@ -1,8 +1,10 @@
-package at.ac.htlstp.sj21k4b1;
+package at.ac.htlstp.sj21k4b;
+
+import at.ac.htlstp.sj21k4b1.Datei;
 
 import java.util.Vector;
 
-public class CSVsumme2 {
+public class CSVsumme {
     public static final String mappe="datasrc/Mappe1.csv";
     public static void main(String[] args) {
         Vector<String> data = Datei.read(mappe);
@@ -11,9 +13,15 @@ public class CSVsumme2 {
             for (int zeile=1;zeile<=data.size();zeile++) {
                 if (zeile>1) {
                     String[] spalten = data.get(zeile-1).split(";");
-
+                    for (String sp:spalten) {
+                        try {
+                            double d = Double.parseDouble(sp.replaceAll(",", "."));
+                            summe += d;
+                        } catch (NumberFormatException e) {}
+                    }
                 }
             }
+            System.out.println("Summe = "+summe);
         } else System.out.println("Datei kann nicht gelesen werden!");
     }
 }
